e18c9ab6325df22e6c00c9bdc592e1a4c75303c3
johnmangam <johnmangam@gmail.com>
2017-06-09 17:11:42 +0530

    Added USFM tags for the ID, Chapter and Verse. [Excluding specail cases]

--- USFMConverter/Converter.cs
+++ USFMConverter/Converter.cs
@@ -4,13 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace USFMConverter
 {
     public class Converter
     {
-        private static short chapterCounter;
-        private static short verseCounter;
+        private bool IsIDAdded;
+        private short chapterCounter=0;
+        private short verseCounter=0;
+        private bool isTitleContainsChapter;
+        private Regex digitsWithDotOrHyphen = new Regex(@"([ \.\-]*(\d+)[ \.\-]*)");
 
         public List<string> ApplyUSFMTagsToFiles(List<string> fileList)
         {
@@ -21,9 +25,18 @@ namespace USFMConverter
                 var newFileName = GetNewFileName(fileName);
                 newFileList.Add(newFileName);
                 ApplyUSFMTags(fileName, newFileName);
+                ResetCounters();
             }
 
+
             return newFileList;
+        }
+
+        private void ResetCounters()
+        {
+            IsIDAdded=false;
+            chapterCounter=0;
+            verseCounter=0;
         }
 
         private bool ApplyUSFMTags(string sourceFilename, string targetFileName)
@@ -39,26 +52,132 @@ namespace USFMConverter
 
         private string GetProcessedLine(string line)
         {
-            if(ChapterNeedsToBeAdded())
+            if (line.Trim() == string.Empty)
             {
-                line = "\\c " + chapterCounter + " " + line;
+                return line;
             }
-            else if(VerseNeedsToBeAdded())
+
+            if (!IsIDAdded)
             {
-                line = "\\v " + verseCounter + " " + line;
+                if (AddIDUSFMTag(ref line))
+                {
+                    return line;
+                }
+            }
+            else
+            {
+                if (AddChapterUSFMTag(ref line))
+                {
+                    return line;
+                }
+                else if (AddVerseUSFMTag(ref line))
+                {
+                    return line;
+                }
             }
 
             return line; 
         }
 
-
-        private bool ChapterNeedsToBeAdded()
+        private bool AddIDUSFMTag(ref string line)
         {
+            if (IsIDAdded)
+            {
+                return false;
+            }
+            else
+            {
+                if (digitsWithDotOrHyphen.IsMatch(line))
+                {
+                        var matches = digitsWithDotOrHyphen.Matches(line);
+                        if (matches.Count > 0)
+                        {
+                            line = digitsWithDotOrHyphen.Replace(line, "");
+                        }
+                        line = "\\id " + line + Environment.NewLine + "\\c " + ++chapterCounter;
+                  
+                }
+                else
+                {
+                    line = "\\id " + line;
+                }
+
+                IsIDAdded = true;
+
+                return true;
+            }
+        }
+
+        private bool AddChapterUSFMTag(ref string line)
+        {
+            if (verseCounter == 0 && chapterCounter == 0)
+            {
+                if (digitsWithDotOrHyphen.IsMatch(line))
+                {
+                    line = digitsWithDotOrHyphen.Replace(line, "");
+
+                    line = "\\c " + ++chapterCounter + " " + line;
+
+                    verseCounter = 1;
+
+                    return true;
+                }
+            }
+            else
+            {
+                // number found and compare with above
+                if (digitsWithDotOrHyphen.IsMatch(line))
+                {
+                    Match matches = Regex.Match(line, digitsWithDotOrHyphen.ToString());
+                    if (matches.Success)
+                    {
+                        // TODO: special case if (groups.Count > 1)
+
+                        var chapterNumber = ushort.Parse(matches.Groups[2].Value);
+
+                        if (chapterNumber == chapterCounter + 1 && chapterNumber != verseCounter + 1)
+                        {
+                            line = digitsWithDotOrHyphen.Replace(line, "");
+                            line = "\\c " + ++chapterCounter + " " + line;
+                            verseCounter = 1;
+                            return true;
+                        }
+                    }
+                    
+                }
+            }
+
             return false;
         }
 
-        private bool VerseNeedsToBeAdded()
+        private bool AddVerseUSFMTag(ref string line)
         {
+            if(IsIDAdded && chapterCounter>0)
+            {
+                // number found and compare with above
+                Match matches = Regex.Match(line, digitsWithDotOrHyphen.ToString());
+
+                if (matches.Success)
+                {
+                    // TODO: special case if (groups.Count > 1) if two are found break them
+                    foreach(Capture capturedNumber in matches.Groups[2].Captures)
+                    {
+                        var verseNumber = ushort.Parse(capturedNumber.Value);
+
+                        if (verseNumber >= verseCounter + 1)
+                        {
+                            line = digitsWithDotOrHyphen.Replace(line, "");
+                            line = "\\v " + ++verseCounter + " " + line;
+                            
+                        }
+                        else
+                        {
+                            // TODO: Special case;
+                        }
+                    }
+                    return true;
+                }
+            }
             return false;
         }
     }
